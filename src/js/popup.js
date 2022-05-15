4ffaef9f44f0b4e2c0642df8a6b7bea2f5bed1ba
川口涼一 <kawaguchi.ryo.0802@gmail.com>
2022-05-15 18:18:54 +0900

    var.1.0

--- src/js/popup.js
+++ src/js/popup.js
@@ -13,48 +13,50 @@ const func = () => {
     // console.log(tab_url);
 
     // 切り替える開発環境分の変数を作成する
-    let devUrl ,
+    let devUrl,
       stgUrl,
-      relUrl;
+      relUrl;ß
 
     // 切り替えるドメインの数だけ配列を作成する
     // corporate
-    const strLink = [[
-        'https://www.google.com/',
-        'https://www.yahoo.co.jp',
-        'https://www.microsoft.com'
-
-    ]],
+    const strLink = [
+        [
+          'https://www.google.com/',
+          'https://www.yahoo.co.jp',
+          'https://www.microsoft.com'ß
+        ]
+      ],
 
       target01 = strLink[0];
-     // console.log(target01);
+    // console.log(target01);
 
-      if (tab_url.includes(target01)) {
-        devUrl = tab_url.replace(/https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/g, 'https://www.google.com/');
-        stgUrl = tab_url.replace(/https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/g, 'https://www.yahoo.co.jp');
-        relUrl = tab_url.replace(/https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/g, 'https://www.microsoft.com');
-      }
+    if (tab_url.includes(target01)) {
+      devUrl = tab_url.replace(/https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/g, 'https://www.google.com/');
+      stgUrl = tab_url.replace(/https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/g, 'https://www.yahoo.co.jp');
+      relUrl = tab_url.replace(/https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/g, 'https://www.microsoft.com');
+    }
 
 
-      // 該当外
-      else {
-        const alertElement = document.getElementById('textWrap'),
-          listElement = document.getElementById('listWrap');
-        alertElement.innerHTML = '<p>該当するページではありません。</p>';
-        listElement.classList.add('hide');
-      };
+    // 該当外
+    /* else {
+      const alertElement = document.getElementById('textWrap'),
+        listElement = document.getElementById('listWrap');
+      alertElement.innerHTML = '<p>該当するページではありません。</p>';
+      listElement.classList.add('hide');
+    } */
+    ;
 
     // URLが書き換えられているか確認する
     // console.log(relUrl,stgUrl,devUrl);
 
     // ポップアップに書き換え後の要素を追加する
-    const relLink = document.getElementById('rel'),
+    /* const relLink = document.getElementById('rel'),
       stgLink = document.getElementById('stg'),
       devLink = document.getElementById('dev');
 
     relLink.insertAdjacentHTML('beforeend', '<dl><dt>Production</dt><dd><a href="' + relUrl + '" target="_blank">' + relUrl + '</a></dd></dl>');
     stgLink.insertAdjacentHTML('beforeend', '<dl><dt>Staging</dt><dd><a href="' + stgUrl + '" target="_blank">' + stgUrl + '</a></dd></dl>');
-    devLink.insertAdjacentHTML('beforeend', '<dl><dt>Development</dt><dd><a href="' + devUrl + '" target="_blank">' + devUrl + '</a></dd></dl>');
+    devLink.insertAdjacentHTML('beforeend', '<dl><dt>Development</dt><dd><a href="' + devUrl + '" target="_blank">' + devUrl + '</a></dd></dl>'); */
   });
 }
 func();
